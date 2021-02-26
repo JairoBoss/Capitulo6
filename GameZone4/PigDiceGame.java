3d68e7a1a8226fadc54880a2da1b51f53a367aa3
Jairo Esteban Martínez Portillo <61020858+JairoBoss@users.noreply.github.com>
2021-02-26 14:52:02 -0600

    .

--- GameZone4/PigDiceGame.java
+++ GameZone4/PigDiceGame.java
@@ -2,16 +2,15 @@ package GameZone4;
 
 public class PigDiceGame {
     public static void main(String[] args) {
-        Jugador jairo = new Jugador();
-        Jugador esteban = new Jugador();
+        Jugador jugador1 = new Jugador();
+        Jugador jugador2 = new Jugador();
 
         do {
-            
-            jairo.jugar();
-        }while (jairo.getValor() <= 20);
-
-
-
+            System.out.println("Jugador1");
+            jugador1.jugar();
+            System.out.println("Jugador2");
+            jugador2.jugar();
+        }while (jugador1.getValor() <= 10 || jugador2.getValor() <= 10);
     }
 
 }
